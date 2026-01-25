42dcc512b50e002a9b0bc26c20b88c8abf07af45
David Peicho <david.peicho@wonderlandengine.com>
2026-01-25 16:18:53 +0100

    tests: Fix bvh8_cpu not splitted

--- tests/tests.rs
+++ tests/tests.rs
@@ -273,7 +273,7 @@ mod tests {
     fn layout_bvh8_cpu() {
         let primitives = split_triangles(1.0);
 
-        let bvh = wald::BVH::new(primitives.as_slice().into()).unwrap();
+        let mut bvh = wald::BVH::new(primitives.as_slice().into()).unwrap();
         bvh.split_leaves(4);
         let mbvh = mbvh::BVH::new(&bvh);
         assert_eq!(mbvh.leaf_count(0), 2);
